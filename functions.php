b0c2993d47a9302368cd29b54553e5503417734b
Tanya <tanhan01@easv365.dk>
2024-04-23 19:06:02 +0200

    Update functions.php

--- functions.php
+++ functions.php
@@ -1,14 +1,20 @@
 <?php
-function enqueue_parent_theme_style() {
-    wp_enqueue_style("style", get_template_directory_uri() . "/style.css"); // Linking stylesheet
-    wp_enqueue_style("adobe", "https://use.typekit.net/slb7mne.css"); // Linking Adobe fonts
+function enqueue_parent_theme_style()
+{
+  wp_enqueue_style("style", get_template_directory_uri() . "/style.css"); // Linking stylesheet
+  wp_enqueue_style("adobe", "https://use.typekit.net/slb7mne.css"); // Linking Adobe fonts
 }
 add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 
-  // Remove gutenberg editor in WP
+// Remove gutenberg editor in WP
 
-  function ad_remove_gutenberg() {
-    remove_post_type_support( "page", "editor" );
-    remove_post_type_support( "post", "editor" );
-  }
-  add_action("init", "ad_remove_gutenberg");
+function ad_remove_gutenberg()
+{
+  remove_post_type_support("page", "editor");
+  remove_post_type_support("post", "editor");
+}
+add_action("init", "ad_remove_gutenberg");
+
+// Removing WooCommerce CSS
+
+add_filter("woocommerce_enqueue_styles", "__return_empty_array");
