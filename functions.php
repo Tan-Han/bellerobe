7ed5d0de7792839c7f0b883855d77f340aa7b532
Tanya <tanhan01@easv365.dk>
2024-04-23 18:28:52 +0200

    Style and functions updated

--- functions.php
+++ functions.php
@@ -1,5 +1,14 @@
 <?php
 function enqueue_parent_theme_style() {
-    wp_enqueue_style('style', get_stylesheet_directory_uri() . '/style.css');
+    wp_enqueue_style("style", get_template_directory_uri() . "/style.css"); // Linking stylesheet
+    wp_enqueue_style("adobe", "https://use.typekit.net/slb7mne.css"); // Linking Adobe fonts
 }
 add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
+
+  // Remove gutenberg editor in WP
+
+  function ad_remove_gutenberg() {
+    remove_post_type_support( "page", "editor" );
+    remove_post_type_support( "post", "editor" );
+  }
+  add_action("init", "ad_remove_gutenberg");
