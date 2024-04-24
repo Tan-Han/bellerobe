bae4dbea004396e348f96b48c04a02c8b26610b3
Tanya <tanhan01@easv365.dk>
2024-04-24 11:26:15 +0200

    cards

--- index.php
+++ index.php
@@ -1,7 +1,8 @@
 <?php get_header() ?>
-
-    <?php while(have_posts()): the_post() ?>
+<main>
+    <?php while (have_posts()):
+        the_post() ?>
         <?php the_content() ?>
     <?php endwhile ?>
-
+</main>
 <?php get_footer() ?>
