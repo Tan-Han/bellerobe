53b921327b0154715994e4426da2889c8438094c
Tanya <tanhan01@easv365.dk>
2024-04-26 16:40:48 +0200

    testing extra stylesheet

--- functions.php
+++ functions.php
@@ -3,6 +3,11 @@ function enqueue_parent_theme_style()
 {
   wp_enqueue_style("style", get_template_directory_uri() . "/style.css"); // Linking stylesheet
   wp_enqueue_style("adobe", "https://use.typekit.net/slb7mne.css"); // Linking Adobe fonts
+
+  // Only styling the product category for bryllupskjoler
+  if(has_term("brudekjoler", "product_cat") ) {
+    wp_enqueue_script("brudekjoler", get_stylesheet_directory_uri() . "/brudekjoler.css");
+  }
 }
 add_action('wp_enqueue_scripts', 'enqueue_parent_theme_style');
 
@@ -19,8 +24,3 @@ add_action("init", "ad_remove_gutenberg");
 
 add_filter("woocommerce_enqueue_styles", "__return_empty_array");
 
-// Only styling the product category for bryllupskjoler
-
-if(has_term("brudekjoler", "product_cat") ) {
-  wp_enqueue_script("brudekjoler", get_stylesheet_directory_uri() . "/brudekjoler.css");
-}
