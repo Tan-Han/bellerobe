0105d3a71169e9edfb2f1d21f1023630d4e6fafb
Tanya <tanhan01@easv365.dk>
2024-04-27 12:44:50 +0200

    image gallery edit

--- index.php
+++ index.php
@@ -6,3 +6,30 @@
     <?php endwhile ?>
 </main>
 <?php get_footer() ?>
+
+<script>
+    $(".woocommerce-product-gallery__image")
+  // tile mouse actions
+  .on("mouseover", function() {
+    $(this)
+      .children(".woocommerce-product-gallery__image img")
+      .css({ transform: "scale(" + $(this).attr("data-scale") + ")" });
+  })
+  .on("mouseout", function() {
+    $(this)
+      .children(".woocommerce-product-gallery__image img")
+      .css({ transform: "scale(1)" });
+  })
+  .on("mousemove", function(e) {
+    $(this)
+      .children(".woocommerce-product-gallery__image img")
+      .css({
+        "transform-origin":
+          ((e.pageX - $(this).offset().left) / $(this).width()) * 100 +
+          "% " +
+          ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +
+          "%"
+      });
+  });
+
+</script>
