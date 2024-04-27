9ab172647d5795fe1f4ed00e3607ecd64427cb83
Tanya <tanhan01@easv365.dk>
2024-04-27 12:49:57 +0200

    test

--- index.php
+++ index.php
@@ -6,32 +6,4 @@
     <?php endwhile ?>
 </main>
 
-
-<script>
-    $(".flex-viewport")
-  // tile mouse actions
-  .on("mouseover", function() {
-    $(this)
-      .children(".woocommerce-product-gallery__image img")
-      .css({ transform: "scale(" + $(this).attr("data-scale") + ")" });
-  })
-  .on("mouseout", function() {
-    $(this)
-      .children(".woocommerce-product-gallery__image img")
-      .css({ transform: "scale(1)" });
-  })
-  .on("mousemove", function(e) {
-    $(this)
-      .children(".woocommerce-product-gallery__image img")
-      .css({
-        "transform-origin":
-          ((e.pageX - $(this).offset().left) / $(this).width()) * 100 +
-          "% " +
-          ((e.pageY - $(this).offset().top) / $(this).height()) * 100 +
-          "%"
-      });
-  });
-
-</script>
-
 <?php get_footer() ?>
