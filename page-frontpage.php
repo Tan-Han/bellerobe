e44d01e2ba078bf3559c931ae97cd58874c3119a
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-04-29 11:17:42 +0200

    adding sections

--- page-frontpage.php
+++ page-frontpage.php
@@ -1,3 +1,37 @@
 <?php get_header() ?>
 
+
+
+<!-- this is the coding for the frontpage for the website-->
+
+<main>
+
+<section class="frontpage_hero">
+
+</section>
+
+<section class="frontpage_introduction">
+
+</section>
+
+<section class="frontpage_categories">
+
+</section>
+
+<div class="frontpage_information">
+  <section class="frontpage_opening_hours_and_booking">
+  </section>
+
+  <section class="frontpage_map">
+  </section>
+</div>
+  
+</main>
+
+
+<!-- this is the styling for the frontpage -->
+<style>
+
+</style>
+
 <?php get_footer() ?>
