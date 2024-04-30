445f2833596dc642396e5ab4e50d99c4ece01c50
Tanya <tanhan01@easv365.dk>
2024-04-30 16:38:08 +0200

    booking changes

--- page-booking.php
+++ page-booking.php
@@ -15,11 +15,6 @@
                     <div class="booking-card-headline">
                         <h3> <?php echo $bookingWeddingDress['booking_headline']; ?> </h3>
 
-                        <?php $bookingPrice = get_sub_field('booking_price')
-                        if (get_sub_field('booking_price')) : ?>
-                            <p>DKK <?php the_sub_field('booking_price'); ?> | </p>
-                        <?php endif: ?>
-                            <p><?php echo $bookingWeddingDress['booking_time'] ?></p>
 
                     </div>
                     <p><?php echo $bookingWeddingDress['booking_description']; ?></p>
