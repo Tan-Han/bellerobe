90eddb899e6d333bd27bd8d2c0be2f2a97173c29
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-04-30 15:28:22 +0200

    comments in coding

--- page-frontpage.php
+++ page-frontpage.php
@@ -1,25 +1,30 @@
 <?php get_header() ?>
 
-
-
-<!-- this is the coding for the frontpage for the website-->
+<!-- This is the coding for the frontpage for the website-->
 
 <main>
 
+<!-- Coding for the first section on the frontpage with the hero video/picture -->
 <section class="frontpage_hero">
   <h1><?php the_field("hero_text_on_frontpage") ?></h1>
-  <?php the_field("hero_on_frontpage") ?>
+  <?php the_field("hero_on_frontpage") 
+    $attributes = 'frameborder="0"';
+    $iframe = str_replace('></iframe>', ' ' . $attributes . '></iframe>', $iframe);
+  ?>
 </section>
 
+<!-- Coding for the second section on the frontpage with the introduction text -->
 <section class="frontpage_introduction">
   <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
   <p><?php the_field("introduction_text_on_frontpage") ?></p>
 </section>
 
+<!-- Coding for the third section on the frontpage with four categories of products -->
 <section class="frontpage_categories">
 
 </section>
 
+<!-- Coding for the fourth section on the frontpage with the informations of opening hours and map -->
 <div class="frontpage_information">
   <section class="frontpage_opening_hours_and_booking">
   </section>
@@ -31,7 +36,7 @@
 </main>
 
 
-<!-- this is the styling for the frontpage -->
+<!-- This is the styling for the frontpage -->
 <style>
 
 </style>
