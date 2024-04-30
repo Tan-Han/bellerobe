b83b9a752993aa3d70f9c67265eac587385ef6e5
Tanya <tanhan01@easv365.dk>
2024-04-30 14:36:43 +0200

    booking page added

--- page-booking.php
+++ page-booking.php
@@ -13,7 +13,7 @@
     window.addEventListener('load', function() {
         calendar.schedulingButton.load({
         url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3dfzNhX3Dbm8eYZF1PHTrTYGqW7ZPnZOdHddNivtZhD6Jk_XccNHxsNS-UHsuHj-VfqhIcBd8w?gv=true',
-        color: 'var(--tertiary-color)',
+        color: '#ff0000',
         label: "Book pr\xF8vning af brudekjole",
         target,
         });
