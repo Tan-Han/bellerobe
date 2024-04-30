881bf627cb6ea4c5186ed1165929e52f450968d5
Tanya <tanhan01@easv365.dk>
2024-04-30 14:35:39 +0200

    booking page added

--- page-booking.php
+++ page-booking.php
@@ -2,36 +2,37 @@
 
 
 <main>
+
+<!-- Knap til brudekjole prøvning -->
     <!-- Google Calendar Appointment Scheduling begin -->
-    <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+    <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet" class="wedding-dress-btn">
     <script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
     <script>
-        (function () {
-            var target = document.currentScript;
-            window.addEventListener('load', function () {
-                calendar.schedulingButton.load({
-                    url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3dfzNhX3Dbm8eYZF1PHTrTYGqW7ZPnZOdHddNivtZhD6Jk_XccNHxsNS-UHsuHj-VfqhIcBd8w?gv=true',
-                    color: '#779a66',
-                    label: "Book pr\xF8vning",
-                    target,
-                });
-            });
-        })();
+    (function() {
+    var target = document.currentScript;
+    window.addEventListener('load', function() {
+        calendar.schedulingButton.load({
+        url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3dfzNhX3Dbm8eYZF1PHTrTYGqW7ZPnZOdHddNivtZhD6Jk_XccNHxsNS-UHsuHj-VfqhIcBd8w?gv=true',
+        color: 'var(--tertiary-color)',
+        label: "Book pr\xF8vning af brudekjole",
+        target,
+        });
+    });
+    })();
     </script>
     <!-- end Google Calendar Appointment Scheduling -->
 
 
 
-    <!-- Google Calendar Appointment Scheduling begin -->
-    <iframe
-        src="https://calendar.google.com/calendar/appointments/schedules/AcZssZ3dfzNhX3Dbm8eYZF1PHTrTYGqW7ZPnZOdHddNivtZhD6Jk_XccNHxsNS-UHsuHj-VfqhIcBd8w?gv=true"
-        style="border: 0" width="100%" height="600" frameborder="0"></iframe>
-    <!-- end Google Calendar Appointment Scheduling -->
+
+
 
 </main>
 
 <style>
-
+.wedding-dress-btn {
+    color: var(--white);
+}
 </style>
 
 <?php get_footer() ?>
