b21b09ddc71999e5860803742ba666eb23ef4b07
Tanya <tanhan01@easv365.dk>
2024-05-02 12:25:22 +0200

    nav changes

--- header.php
+++ header.php
@@ -1,19 +1,22 @@
 <!DOCTYPE html>
 <html lang="en">
+
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
     <title><?php bloginfo("name") ?></title>
     <?php wp_head() ?>
 </head>
+
 <body>
-<div class="header">
+
     <div class="logo-top">
-        <img src="https://trshansen.online/bellerobe/wp-content/themes/bellerobe/img/Belle%20Robe%20logo.svg" alt="" class="logo">
+        <img src="https://trshansen.online/bellerobe/wp-content/themes/bellerobe/img/Belle%20Robe%20logo.svg" alt=""
+            class="logo">
     </div>
     <nav>
         <ul>
-            <li><a href="/frontpage">Forside</a></li>    
+            <li><a href="/frontpage">Forside</a></li>
             <li><a href="/product-category/brudekjoler">Brudekjoler</a></li>
             <li><a href="/product-category/gallakjoler">Fest- & Gallakjoler</a></li>
             <li><a href="/product-category/konfirmationskjoler">Konfirmationskjoler</a></li>
@@ -23,4 +26,3 @@
         </ul>
         <a href="/cart" class="cart-btn"><button>Kurv</button></a>
     </nav>
-</div>
