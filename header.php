15046f7b5794c3e02dc070b9ace1c0d6ba8c6ded
Tanya <tanhan01@easv365.dk>
2024-05-05 17:03:38 +0200

    shopping basket btn

--- header.php
+++ header.php
@@ -28,7 +28,6 @@
         <div class="cart-btn-box">
             <div class="cart-count">
                 <?php echo count_item_in_cart(); ?>
-
             </div>
             <a href="/cart/" class="cart-btn">Kurv<i class='fas fa-shopping-basket'></i></a>
         </div>
