b92f5db10d09cb3f5327ecaf44e4eed64599cbfc
Tanya <tanhan01@easv365.dk>
2024-05-05 11:40:06 +0200

    contact info

--- page-booking.php
+++ page-booking.php
@@ -319,8 +319,8 @@
         </div>
         <div class="contact-info">
             <a href="tel:<?php the_field("phone_number") ?>"><?php the_field("phone_number") ?></a>
-            <button id="contactForm">Send en besked</button>
-            <div class="contact_form_box">
+            <button id="contactBtn">Send en besked</button>
+            <div id="contactForm" class="contact_form_box">
                 <div class="contact_form">
                     <span class="close">&times;</span>
                     <?php echo do_shortcode('[contact-form-7 id="8a0404d" title="Belle Robe Contact Form"]') ?>
@@ -346,6 +346,34 @@
     </section>
 
 </main>
+
+<script>
+    // Get the modal
+    var modal = document.getElementById("contactFrom");
+
+    // Get the button that opens the modal
+    var btn = document.getElementById("contactBtn");
+
+    // Get the <span> element that closes the modal
+    var span = document.getElementsByClassName("close")[0];
+
+    // When the user clicks on the button, open the modal
+    btn.onclick = function () {
+        modal.style.display = "block";
+    }
+
+    // When the user clicks on <span> (x), close the modal
+    span.onclick = function () {
+        modal.style.display = "none";
+    }
+
+    // When the user clicks anywhere outside of the modal, close it
+    window.onclick = function (event) {
+        if (event.target == modal) {
+            modal.style.display = "none";
+        }
+    } 
+</script>
 
 <style>
     main {
