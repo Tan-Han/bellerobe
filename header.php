8a5a0aee2afc391996253d1503acc30ff1df63a8
Tanya <tanhan01@easv365.dk>
2024-05-05 17:21:47 +0200

    shopping basket btn

--- header.php
+++ header.php
@@ -25,9 +25,11 @@
             <li><a href="/om_os/">Om os</a></li>
             <li><a href="/booking/">Book tid</a></li>
         </ul>
-        <div class="cart-btn-box">
-            <span><?php echo count_item_in_cart() ?></span>
-            <a href="/cart/" class="cart-btn">Kurv<i class='fas fa-shopping-basket'></i></a>
+        <div class="cart-btn-outer-box">
+            <div class="cart-btn-box">
+                <span id="cartCount"><?php echo count_item_in_cart() ?></span>
+                <a href="/cart/" class="cart-btn">Kurv<i class='fas fa-shopping-basket'></i></a>
+            </div>
         </div>
 
     </nav>
