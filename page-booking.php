c804b13fde2f32752fbd9286f33475a5a65eb2a8
Tanya <tanhan01@easv365.dk>
2024-05-05 13:58:01 +0200

    contact info

--- page-booking.php
+++ page-booking.php
@@ -706,7 +706,7 @@
         border-radius: 10px;
         border: 1px solid var(--quaternary-color) !important;
         box-shadow: 3px 3px 5px #5C48337D;
-        height: 400px;
+        height: 350px;
     }
 </style>
 
