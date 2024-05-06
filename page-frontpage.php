508801d6d1eb38be12357ac2c6f4546a1ab1ac80
Tanya <tanhan01@easv365.dk>
2024-05-06 21:38:34 +0200

    Frontpage - Tanya

--- page-frontpage.php
+++ page-frontpage.php
@@ -4,73 +4,98 @@
 
 <main>
 
-<!-- coding for the first section on the frontpage with the hero video/picture -->
-<section class="frontpage_hero">
-  <h1><?php the_field("hero_text_on_frontpage") ?></h1>
-  <div class="hero">
-    <?php the_field("hero_on_frontpage") ?>
-  </div>
-</section>
-
-<!-- coding for the second section on the frontpage with the introduction text -->
-<section class="section frontpage_introduction">
-  <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
-  <p><?php the_field("introduction_text_on_frontpage") ?></p>
-</section>
-
-<!-- coding for the third section on the frontpage with four categories of products -->
-<section class="section frontpage_categories">
-  <h2><?php the_field("introduction_to_cards_on_frontpage") ?></h2>
-  
-  <div>
-    <div class="frontpage_cards">
-      <a href="/product-category/brudekjoler/">Brudekjoler</a>
-    </div>
-
-    <div class="frontpage_cards">
-      <a href="/product-category/gallakjoler/">Galla & festkjoler</a>
-    </div>
-
-    <div class="frontpage_cards">
-      <a href="/product-category/konfirmationskjoler/">Konfirmationskjoler</a>
-    </div>
-
-    <div class="frontpage_cards">
-      <a href="/product-category/sko/">Sko & tilbehør</a>
-    </div>
-  </div>
-</section>
-
-<!-- coding for the fourth section on the frontpage with the informations of opening hours and map -->
-<div class="frontpage_information">
-  
-  <section class="section frontpage_opening_hours_and_booking">
+  <!-- coding for the first section on the frontpage with the hero video/picture -->
+  <section class="frontpage_hero">
+    <h1><?php the_field("hero_text_on_frontpage") ?></h1>
+    <iframe src="<?php the_field("hero_on_frontpage") ?>?rel=0&autoplay=1&controls=0&showinfo=0" frameborder="0"></iframe> <!-- Har slettet div, da den var unødvendig og lavet den om til URL i ACF. Jeg har tilføjet lidt kode efter linket for at autoplay, loop og fjerne YT controls-->
   </section>
 
-  <section class="frontpage_map">
+  <!-- coding for the second section on the frontpage with the introduction text -->
+  <section class="section frontpage_introduction">
+    <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
+    <p><?php the_field("introduction_text_on_frontpage") ?></p>
   </section>
 
-</div>
-  
+  <!-- coding for the third section on the frontpage with four categories of products -->
+  <section class="section frontpage_categories">
+    <h2><?php the_field("introduction_to_cards_on_frontpage") ?></h2>
+
+    <div>
+      <div class="frontpage_cards">
+        <a href="/product-category/brudekjoler/">Brudekjoler</a>
+      </div>
+
+      <div class="frontpage_cards">
+        <a href="/product-category/gallakjoler/">Galla & festkjoler</a>
+      </div>
+
+      <div class="frontpage_cards">
+        <a href="/product-category/konfirmationskjoler/">Konfirmationskjoler</a>
+      </div>
+
+      <div class="frontpage_cards">
+        <a href="/product-category/sko/">Sko & tilbehør</a>
+      </div>
+    </div>
+  </section>
+
+  <!-- coding for the fourth section on the frontpage with the informations of opening hours and map -->
+  <div class="frontpage_information">
+
+    <section class="section frontpage_opening_hours_and_booking">
+    </section>
+
+    <section class="frontpage_map">
+    </section>
+
+  </div>
+
 </main>
 
 
 <!-- this is the styling for the frontpage -->
 <style>
-  .hero {
-    width: 100vw;
-    background-size: cover;
-    background-position: center;
+  .main {
+    padding: 0;
+    /* Har sat padding til 0 for at kunne sætte videoen helt ud til kanten */
+  }
+
+  .frontpage_hero {
+    position: relative;
+    /* For at kunne sætte teksen oven på videoen */
+    width: 100%;
+  }
+
+  .frontpage_hero h1 {
+    position: absolute;
+    /* For at kunne sætte teksen oven på videoen */
+    z-index: 1;
+    /* For at kunne sætte teksen oven på videoen */
+    top: 50%;
+    /* For at placere tekst midt i */
+    left: 50%;
+    /* For at placere tekst midt i */
+    transform: translate(-50%, -50%);
+    /* For at placere tekst midt i */
+    text-shadow: 3px 3px 5px var(--brown);
+  color: var(--white);
+  font-size: 55px;
+  }
+
+  .frontpage_hero iframe {
+    max-width: 100vw;
+    /* Max-width af video - for at den ikke går ud over kanten */
+    height: 600px;
+    /* Sæt højden som ønsket - 600px er bare et eksempel */
+    width: 100%;
+    /* 100% vidde på videoen, så den fylder fra kant til kant */
   }
 
   .section {
     padding: 20px 0px;
   }
 
-  .frontpage_cards {
-    
-  }
-
+  .frontpage_cards {}
 </style>
 
 <?php get_footer() ?>
