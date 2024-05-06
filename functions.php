d2d5b351efec5396da1f9decbda6c42c285b2055
Tanya <tanhan01@easv365.dk>
2024-05-06 10:15:46 +0200

    footer

--- functions.php
+++ functions.php
@@ -87,7 +87,6 @@ class My_ACF_Location_Footer extends ACF_Location
     $this->name = 'footer';
     $this->label = __("Footer", 'acf');
     $this->category = 'page';
-    $this->object_type = 'footer';
   }
 }
 
