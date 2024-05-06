10cd654872d4db111afd1ded17565c5e0508bd8a
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-05-06 17:00:34 +0200

    comments

--- page-frontpage.php
+++ page-frontpage.php
@@ -5,7 +5,7 @@
 <main>
 
 <!-- coding for the first section on the frontpage with the hero video/picture -->
-<section class="frontpage_hero">
+<section class="section frontpage_hero">
   <h1><?php the_field("hero_text_on_frontpage") ?></h1>
   <?php the_field("hero_on_frontpage") 
   
@@ -13,13 +13,13 @@
 </section>
 
 <!-- coding for the second section on the frontpage with the introduction text -->
-<section class="frontpage_introduction">
+<section class="section frontpage_introduction">
   <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
   <p><?php the_field("introduction_text_on_frontpage") ?></p>
 </section>
 
 <!-- coding for the third section on the frontpage with four categories of products -->
-<section class="frontpage_categories">
+<section class="section frontpage_categories">
   <h2><?php the_field("introduction_to_cards_on_frontpage") ?></h2>
   
 </section>
@@ -27,7 +27,7 @@
 <!-- coding for the fourth section on the frontpage with the informations of opening hours and map -->
 <div class="frontpage_information">
   
-  <section class="frontpage_opening_hours_and_booking">
+  <section class="section frontpage_opening_hours_and_booking">
   </section>
 
   <section class="frontpage_map">
@@ -40,6 +40,9 @@
 
 <!-- this is the styling for the frontpage -->
 <style>
+  .section {
+    padding: 20px 0px;
+  }
 
 </style>
 
