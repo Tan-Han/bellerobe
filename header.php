5eef3eb8817559f0e35086a2881327284100fabe
Tanya <tanhan01@easv365.dk>
2024-05-07 20:40:46 +0200

    loho header

--- header.php
+++ header.php
@@ -13,22 +13,23 @@
     <div class="logo-top">
         <img src="https://trshansen.online/bellerobe/wp-content/themes/bellerobe/img/logo-bellerobe.svg" alt=""
             class="logo">
-    </div>
-    <nav>
-        <ul>
-            <li><a href="/">Forside</a></li>
-            <li><a href="/product-category/brudekjoler/">Brudekjoler</a></li>
-            <li><a href="/product-category/gallakjoler/">Fest- & Gallakjoler</a></li>
-            <li><a href="/product-category/konfirmationskjoler/">Konfirmationskjoler</a></li>
-            <li><a href="/product-category/sko/">Sko</a></li>
-            <li><a href="/om_os/">Om os</a></li>
-            <li><a href="/booking/">Book tid</a></li>
-        </ul>
-        <div class="cart-btn-outer-box">
-            <div class="cart-btn-box">
-                <span id="cartCount"><?php echo count_item_in_cart() ?></span>
-                <a href="/cart/" class="cart-btn">Kurv<i class='fas fa-shopping-basket'></i></a>
-            </div>
-        </div>
 
-    </nav>
+        <nav>
+            <ul>
+                <li><a href="/">Forside</a></li>
+                <li><a href="/product-category/brudekjoler/">Brudekjoler</a></li>
+                <li><a href="/product-category/gallakjoler/">Fest- & Gallakjoler</a></li>
+                <li><a href="/product-category/konfirmationskjoler/">Konfirmationskjoler</a></li>
+                <li><a href="/product-category/sko/">Sko</a></li>
+                <li><a href="/om_os/">Om os</a></li>
+                <li><a href="/booking/">Book tid</a></li>
+            </ul>
+            <div class="cart-btn-outer-box">
+                <div class="cart-btn-box">
+                    <span id="cartCount"><?php echo count_item_in_cart() ?></span>
+                    <a href="/cart/" class="cart-btn">Kurv<i class='fas fa-shopping-basket'></i></a>
+                </div>
+            </div>
+
+        </nav>
+    </div>
