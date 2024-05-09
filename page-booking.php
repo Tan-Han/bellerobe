d2d3f2c2c2a9e12770396ec55fb06e868d11a559
Tanya <tanhan01@easv365.dk>
2024-05-09 12:12:37 +0200

    booking

--- page-booking.php
+++ page-booking.php
@@ -9,6 +9,51 @@
 
             <!-- Wedding dress booking -->
 
+            <div class="booking-card">
+                <?php
+                $bookingDress = get_field('provning_brudekjole');
+                if ($bookingDress): ?>
+
+                    <div class="img-box">
+                        <img class="booking-img"
+                            src="<?php echo esc_url($bookingDress['dress_img']['sizes']['medium']); ?>" alt="">
+                    </div>
+                    <div class="booking-info">
+                        <div class="booking-card-headline">
+                            <h3><?php echo $bookingDress['booking_headline']; ?></h3>
+
+                            <div class="price-time">
+                                <?php if (!empty($bookingDress['booking_price'])): ?>
+                                    <p>DKK <?php echo $bookingDress['booking_price']; ?> |&nbsp;</p>
+                                <?php endif ?>
+                                <p><?php echo $bookingDress['booking_time'] ?></p>
+                            </div>
+
+                        </div>
+                        <p class="booking-description"><?php echo $bookingDress['booking_description']; ?></p>
+                        <div class="booking-btn">
+                            <!-- Google Calendar Appointment Scheduling begin -->
+                            <link href="https://calendar.google.com/calendar/scheduling-button-script.css" rel="stylesheet">
+                            <script src="https://calendar.google.com/calendar/scheduling-button-script.js" async></script>
+                            <script>
+                                (function () {
+                                    var target = document.currentScript;
+                                    window.addEventListener('load', function () {
+                                        calendar.schedulingButton.load({
+                                            url: 'https://calendar.google.com/calendar/appointments/schedules/AcZssZ3rgkfC1es-OaLCs0claeBuIIy4neu6WscyHa7LYYbgm8ulq4kQINGqZuL8PLHzoya-HcC_aiEM?gv=true',
+                                            color: '#039BE5',
+                                            label: "Book pr\xF8vning",
+                                            target,
+                                        });
+                                    });
+                                })();
+                            </script>
+                            <!-- end Google Calendar Appointment Scheduling -->
+                        </div>
+                    </div>
+                <?php endif; ?>
+            </div>
+
             <!-- Outlet Wedding dress booking -->
 
             <div class="booking-card">
