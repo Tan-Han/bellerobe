d715a31be1a9ccf1acf83b9c5bf53dca4f58f3b6
Tanya <tanhan01@easv365.dk>
2024-05-09 12:29:13 +0200

    booking

--- page-booking.php
+++ page-booking.php
@@ -774,7 +774,7 @@
 
     .maps {
         border-radius: 10px;
-        border: 1px solid var(--quaternary-color) !important;
+        border: none !important;
         box-shadow: 3px 3px 5px #5C48337D;
         height: 350px;
         width: 100%;
