c4e20bbe359b5784c8c1e665b4c0a65d18ff1b17
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-05-10 14:49:43 +0200

    setting up third section

--- page-frontpage.php
+++ page-frontpage.php
@@ -4,25 +4,29 @@
 
 <main>
 
-  <!-- coding for the first section on the frontpage with the hero video/picture -->
-  <section class="frontpage_hero">
-    <h1><?php the_field("hero_text_on_frontpage") ?></h1>
-    <iframe
-      src="<?php the_field("hero_on_frontpage") ?>?autoplay=1&controls=0&mute=1&modestbranding=1&disablekb=1&loop=1&playlist=<?php the_field("video_id")?>" title="YouTube video player" frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen></iframe>
-    <!-- Har slettet div, da den var unødvendig og lavet den om til URL i ACF. Jeg har tilføjet lidt kode efter linket for at autoplay, loop og fjerne YT controls -->
+    <!-- first section on the frontpage with the hero video/picture -->
+    <section class="frontpage_hero">
+      <h1><?php the_field("hero_text_on_frontpage") ?></h1>
+      <iframe
+        src="<?php the_field("hero_on_frontpage") ?>?autoplay=1&controls=0&mute=1&modestbranding=1&disablekb=1&loop=1&playlist=<?php the_field("video_id")?>" title="YouTube video player" frameborder="0"
+        allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+        referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      </iframe>
+      <!-- Har slettet div, da den var unødvendig og lavet den om til URL i ACF. Der er tilføjet lidt kode efter linket for at autoplay, loop og fjerne Youtube controls -->
+    </section>
 
-<div class="frontpage_pading">
-<!-- coding for the second section on the frontpage with the introduction text -->
-<section class="section frontpage_introduction">
-  <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
-  <p><?php the_field("introduction_text_on_frontpage") ?></p>
-</section>
+  <!--padding for på siderne for alt andet end hero -->
+  <div class="frontpage_pading">
 
-<!-- coding for the third section on the frontpage with four categories of products -->
+    <!-- second section on the frontpage with the introduction text -->
+    <section class="section frontpage_introduction">
+      <h2><?php the_field("introduction_header_on_frontpage") ?></h2>
+      <p><?php the_field("introduction_text_on_frontpage") ?></p>
+    </section>
+
+<!-- third section on the frontpage with four categories of products -->
 <section class="section frontpage_categories">
-  <h2><?php the_field("introduction_to_cards_on_frontpage") ?></h2>
+  <h2 class="centered_text_frontpage"><?php the_field("introduction_to_cards_on_frontpage") ?></h2>
   
   <div>
     <div class="frontpage_cards">
@@ -43,7 +47,7 @@
   </div>
 </section>
 
-<!-- coding for the fourth section on the frontpage with the informations of opening hours and map -->
+<!-- fourth section on the frontpage with the informations of opening hours and map -->
 <div class="frontpage_information">
   
   <section class="section frontpage_opening_hours_and_booking">
@@ -58,10 +62,21 @@
 
 <!-- this is the styling for the frontpage -->
 <style>
+
   main {
     padding: 0 !important;
     /* Har sat padding til 0 for at kunne sætte videoen helt ud til kanten (!important for at den overwriter den anden padding)*/
   }
+
+  .section {
+    padding: 20px 0px;
+  }
+
+  .frontpage_pading {
+    padding: 0 8rem;
+  }
+
+  /* First section styling */
 
   .frontpage_hero {
     position: relative;
@@ -94,23 +109,21 @@
     /* 100% vidde på videoen, så den fylder fra kant til kant */
   }
 
-  .frontpage_introduction, .frontpage_categories {
-    padding: 0 8rem;
-  }
+  /* second section styling */
 
-  .section {
-    padding: 20px 0px;
-  }
+  /* Third section styling */
 
-  .frontpage_pading {
-    padding: 0 8rem;
+  .centered_text_frontpage {
+    text-align: center;
   }
 
   .frontpage_cards {
 
   }
 
-  .frontpage_cards {}
+  /* Fourth section styling */
+
+  
 </style>
 
 <?php get_footer() ?>
