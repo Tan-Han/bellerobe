af0b6d459cc3740f96f236afd7b0b5af9cf0e7e0
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-05-11 23:36:59 +0200

    om os siden begyndelse

--- page-om_os.php
+++ page-om_os.php
@@ -1,6 +1,21 @@
 <?php get_header() ?>
 
 <main>
+<!--first section on about us page with text and video-->
+    <section class="introduction_text_and_video">
+        <div class="introduction_text">
+            <h1><?php the_field("introduction_header_on_frontpage") ?></h1>
+            <p><?php the_field("introduction_text_on_frontpage") ?></p>
+        </div>
+        <div class="introduction_video">
+        
+        </div>
+    </section>
+
+<!-- second section on about us page with drop down menues -->
+    <section class="information_drop_downs">
+
+    </section>
 
 </main>
 
