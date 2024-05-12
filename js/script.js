e7490dac201446ae85db0bfeeec5cdb85ebb7676
Tanya <tanhan01@easv365.dk>
2024-05-12 15:04:17 +0200

    script changes

--- js/script.js
+++ js/script.js
@@ -32,14 +32,12 @@ function dropdownMenuFestGallakjoler() {
 }
 
 // Close the dropdown if the user clicks outside of it
-window.addEventListener('click', function (event) {
-    if (!event.target.matches('.dropbtn')) {
-        var dropdowns = document.getElementsByClassName("dropdown-content");
-        for (var i = 0; i < dropdowns.length; i++) {
-            var dropdown = dropdowns[i];
-            if (dropdown.classList.contains('show')) {
-                dropdown.classList.remove('show');
-            }
+window.addEventListener('click', function(event) {
+    var dropdowns = document.getElementsByClassName("dropdown-content");
+    for (var i = 0; i < dropdowns.length; i++) {
+        var dropdown = dropdowns[i];
+        if (!event.target.matches('.dropbtn') && dropdown.classList.contains('show')) {
+            dropdown.classList.remove('show');
         }
     }
 });
