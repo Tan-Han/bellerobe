785c71c9a250983cfb85959afd511a28860ed903
Ester-Helth <118178505+Ester-Helth@users.noreply.github.com>
2024-05-13 13:26:35 +0200

    editing

--- page-om_os.php
+++ page-om_os.php
@@ -15,8 +15,9 @@
             <iframe
                 src="<?php the_field("about_us_video") ?>?autoplay=1&controls=0&mute=1&modestbranding=1&disablekb=1&loop=1&playlist=<?php the_field("video_id") ?>"
                 title="YouTube video player" frameborder="0"
-                allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
+                allow="accelerometer; controls; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
                 referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+
             </iframe>
         </div>
 
