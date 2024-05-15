8dd4c29c484b5510f24b7996c6aee4fb4fca86f5
Tanya <tanhan01@easv365.dk>
2024-05-15 09:49:45 +0200

    script changes

--- js/script.js
+++ js/script.js
@@ -10,19 +10,22 @@ function mobileMenu() {
 }
 
 window.onclick = function(event) {
+    var screenWidth = window.innerWidth;
     var menu = document.getElementById("menuLinks");
     var icon = document.getElementById("menuIcon");
 
-    // Check if the screen width is below 1150px and if the clicked element is not the menu icon
-    if (screenWidth <= 1150 && event.target !== icon) {
-        // If the clicked element is a menu link, return without closing the menu
-        if (event.target.closest("#menuLinks")) {
-            return;
+    // Check if the screen width is below 1150px
+    if (screenWidth <= 1150) {
+        // If the clicked element is not the menu icon
+        if (event.target !== icon) {
+            // If the menu is open and the clicked element is outside the menu, close the menu
+            if (menu.style.display === "block" && !event.target.closest("#menuLinks")) {
+                menu.style.display = "none";
+            }
         }
-        // Close the menu if clicked outside the menu or menu icon
-        menu.style.display = "none";
     }
 }
+
 
 // DROPDOWN MENU
 
