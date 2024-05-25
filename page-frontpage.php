073c49ee1e2e8fc34d7dedc67a419bfbfa0c8a4e
Tanya <tanhan01@easv365.dk>
2024-05-25 18:35:22 +0200

    frontpage video

--- page-frontpage.php
+++ page-frontpage.php
@@ -6,10 +6,9 @@
 
   <!-- first section on the frontpage with the hero video/picture -->
   <section class="frontpage_hero">
-    <iframe src="<?php the_field("hero_on_frontpage") ?>?autoplay=1&loop=1&controls=0&muted=1&showinfo=0"
+    <iframe src="https://bellerobe.trshansen.online/wp-content/uploads/2024/05/stemmings.mp4?autoplay=1&muted=1&loop=1&controls=0&showinfo=0"
       title="Belle Robe - Stemningsvideo" frameborder="0"
-      allow="accelerometer; autoplay; clipboard-write; encrypted-media; gyroscope; picture-in-picture; web-share"
-      referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
+      allow="autoplay" referrerpolicy="strict-origin-when-cross-origin" allowfullscreen>
     </iframe>
     <!-- kode efter linket gør, at man kan autoplay og fjerne Youtube controls -->
   </section>
