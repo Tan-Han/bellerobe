4ff57dd532406768b3745b2d17b4d14960192f2a
Tanya <tanhan01@easv365.dk>
2024-05-25 18:55:27 +0200

    frontpage video

--- page-frontpage.php
+++ page-frontpage.php
@@ -7,7 +7,7 @@
   <!-- first section on the frontpage with the hero video/picture -->
   <section class="frontpage_hero">
     <video class="frontpage-video">
-      <source scr="/wp-content/uploads/2024/05/stemmings.mp4">
+      <source scr="<?php echo get_field("hero_on_frontpage") ?>">
     </video>
     <!-- kode efter linket gør, at man kan autoplay og fjerne Youtube controls -->
   </section>
